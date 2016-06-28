4e1acad718afd6348dd06889619fc4c9d163c4c7
George Christoforakis <georgechristoforakis1972@gmail.com>
2016-06-28 15:17:05 +0300

    Add project files.

---
+++ ConsoleApp_Local/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Local
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Line 1");
+            Console.WriteLine("Line 2");
+            Console.WriteLine("line lowercase 3");
+
+            Console.WriteLine("Branch 1, Line 1");
+            Console.WriteLine("Branch 2, Line 2");
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
